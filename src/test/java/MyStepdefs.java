4375ff7663b042e3d8a97112df57742de4acc47d
Swithel <80590272+Swithel@users.noreply.github.com>
2023-02-27 11:31:19 +0100

    Initial commit

--- src/test/java/MyStepdefs.java
+++ src/test/java/MyStepdefs.java
@@ -1,7 +1,27 @@
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 
 public class MyStepdefs {
     @Given("I bought the correct ingredients")
     public void iBoughtTheCorrectIngredients() {
+    }
+
+    @Given("I have a functioning kitchen")
+    public void iHaveAFunctioningKitchen() {
+
+    }
+
+    @Given("its friday")
+    public void itsFriday() {
+
+    }
+
+    @When("I prepare the food")
+    public void iPrepareTheFood() {
+    }
+
+    @Then("I can eat Tacos with my family")
+    public void iCanEatTacosWithMyFamily() {
     }
 }
